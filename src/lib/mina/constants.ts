1be40c602a8c7402a8088f908076ba6a95bad75e
Adrian Nagy <adrian.nagy@viablesystems.io>
2023-06-28 12:57:11 +0200

    Increase transactions limit

--- src/lib/mina/constants.ts
+++ src/lib/mina/constants.ts
@@ -16,7 +16,7 @@ export namespace TransactionCost {
   export const SIGNED_SINGLE_COST = 9.14 as const;
 
   // Defined in https://github.com/MinaProtocol/mina/blob/e39abf79b7fdf96717eb8a8ee88ec42ba1e2663d/src/lib/mina_compile_config/mina_compile_config.ml#L73
-  export const COST_LIMIT = 69.45 as const;
+  export const COST_LIMIT = 1000.0 as const;
 }
 
 // Constants to define the maximum number of events and actions in a transaction
